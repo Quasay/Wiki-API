1bf6f1b6fd3c978a35d91ebf1d6b5453a8fdca9f
Quasay <quasaym@gmail.com>
2020-04-30 16:11:50 -0400

    Added chained route handlers

--- app.js
+++ app.js
@@ -23,44 +23,42 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newTitle = req.body.title;
+    const newContent = req.body.content;
+    const newArticle = new Article({
+      title: newTitle,
+      content: newContent,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("It worked!");
+      } else {
+        res.send("Something went wrong :(");
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully Delete all articles");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
-
-app.post("/articles", function (req, res) {
-  const newTitle = req.body.title;
-  const newContent = req.body.content;
-
-  const newArticle = new Article({
-    title: newTitle,
-    content: newContent,
-  });
-
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send("It worked!");
-    } else {
-      res.send("Something went wrong :(");
-      res.send(err);
-    }
-  });
-});
-
-app.delete("/articles", function (req, res) {
-  Article.deleteMany(function (err) {
-    if (!err) {
-      res.send("Successfully Delete all articles");
-    } else {
-      res.send(err);
-    }
-  });
-});
 
 app.listen(3000, function () {
   console.log("Server Started on Port 3000");
