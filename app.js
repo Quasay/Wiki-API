314c8af4978cd658962e2e580503ebf4f61c225c
Quasay <quasaym@gmail.com>
2020-04-30 16:00:41 -0400

    Added Create,Read,Delete

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,4 +23,45 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/", function (req, res) {});
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", function (req, res) {
+  const newTitle = req.body.title;
+  const newContent = req.body.content;
+
+  const newArticle = new Article({
+    title: newTitle,
+    content: newContent,
+  });
+
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send("It worked!");
+    } else {
+      res.send("Something went wrong :(");
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function (req, res) {
+  Article.deleteMany(function (err) {
+    if (!err) {
+      res.send("Successfully Delete all articles");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server Started on Port 3000");
+});
